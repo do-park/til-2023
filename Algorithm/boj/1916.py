aaa1a357a892d274775b8a1510faef116cd5244c
do-park <doheepa@kakao.com>
2023-03-26 23:33:27 +0900

    23-03-25 (solved)

---
+++ Algorithm/boj/1916.py
@@ -0,0 +1,31 @@
+from heapq import heappush, heappop
+
+
+INF = float('inf')
+N = int(input())
+M = int(input())
+hq = []
+routes = [[] for _ in range(N)]
+result = [INF for _ in range(N)]
+for _ in range(M):
+    u, v, w = list(map(int, input().split()))
+    routes[u-1].append((w, v-1))
+start, end = map(int, input().split())
+start, end = start-1, end-1
+
+result[start] = 0
+for route in routes[start]:
+    heappush(hq, route)
+
+while hq:
+    cost, dist = heappop(hq)
+    if result[dist] < cost:
+        continue
+    result[dist] = cost
+    for (c, d) in routes[dist]:
+        c += cost
+        if result[d] > c:
+            result[d] = c
+            heappush(hq, (c, d))
+
+print(result[end])
